1d82cf275e14526ab813b8f5d75b5947bdda56dd
Augustine <augustineelliotten@gmail.com>
2026-03-09 07:23:07 +0800

    upd

--- rhino8-internal-wire/rhino builds/v13/rh8/src/internal wire/AssemblyInfo.cs
+++ rhino8-internal-wire/rhino builds/v13/rh8/src/internal wire/AssemblyInfo.cs
@@ -25,8 +25,8 @@ using Rhino.PlugIns;
 
 [assembly: ComVisible(false)]
 [assembly: Guid("17b2b8e4-030d-4607-ace4-9004a6203486")]
-[assembly: AssemblyVersion("0.1.12.12843")]
-[assembly: AssemblyFileVersion("0.1.12.12843")]
-[assembly: AssemblyInformationalVersion("0.1.12.12843")]
+[assembly: AssemblyVersion("0.1.12.13290")]
+[assembly: AssemblyFileVersion("0.1.12.13290")]
+[assembly: AssemblyInformationalVersion("0.1.12.13290")]
 
 [assembly: TargetFramework(".NETFramework,Version=v4.8", FrameworkDisplayName = ".NET Framework 4.8")]
