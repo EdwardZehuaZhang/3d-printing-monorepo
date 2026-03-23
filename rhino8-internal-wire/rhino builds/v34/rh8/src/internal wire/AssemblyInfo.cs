e4f4d52d71f637fc1daaaa82ac569d0d83db9b45
Augustine <augustineelliotten@gmail.com>
2026-03-23 18:44:39 +0800

    ups

---
+++ rhino8-internal-wire/rhino builds/v34/rh8/src/internal wire/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Reflection;
+using System.Runtime.Versioning;
+using System.Runtime.InteropServices;
+
+using Rhino.PlugIns;
+
+[assembly: PlugInDescription(DescriptionType.Email, "augustineelliotten@gmail.com")]
+[assembly: PlugInDescription(DescriptionType.Phone, "")]
+[assembly: PlugInDescription(DescriptionType.Organization, @"")]
+[assembly: PlugInDescription(DescriptionType.Address, @"")]
+[assembly: PlugInDescription(DescriptionType.Country, @"")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "")]
+[assembly: PlugInDescription(DescriptionType.Icon, "internal wire.Resources.projectIcon.ico")]
+
+[assembly: AssemblyTitle(@"internal wire")]
+[assembly: AssemblyDescription(@"test of 3d printed condictive wiring")]
+[assembly: AssemblyCompany(@"")]
+[assembly: AssemblyProduct(@"internal wire")]
+[assembly: AssemblyCopyright(@"")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("17b2b8e4-030d-4607-ace4-9004a6203486")]
+[assembly: AssemblyVersion("0.4.6.33735")]
+[assembly: AssemblyFileVersion("0.4.6.33735")]
+[assembly: AssemblyInformationalVersion("0.4.6.33735")]
+
+[assembly: TargetFramework(".NETFramework,Version=v4.8", FrameworkDisplayName = ".NET Framework 4.8")]
